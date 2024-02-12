f8ff0477ad7de343ca5a0eb3082361a73340d5cc
Bhargav Gudapati <bhargav_gudapati@yahoo.com>
2024-02-12 13:32:12 -0500

    lib files should be in a lib folder

--- IntegratedGyroAndAccel/IntegratedGyroAndAccel.ino
+++ IntegratedGyroAndAccel/IntegratedGyroAndAccel.ino
@@ -4,12 +4,11 @@
 // MS3 under Advanced Engine, "Generic Sensor Inputs", "CAN ADC01", Sensor 01, RAW
 // setup gauge to display Sensor 01
 
-#include <SPI.h>          //Library for using SPI Communication 
+#include <SPI.h>          //Library for using SPI Communication
+#include <Wire.h>         //this arduino board is fine using Wire, so we can just go ahead and unconditionally import this
 #include <mcp2515.h>      //Library for using CAN Communication
-#include "src/MPU6050.h"  //library is downloaded from online
-#include "src/I2Cdev.h"   //library is downloaded from online
-#include "Wire.h"         //this arduino board is fine using Wire, so we can just go ahead and unconditionally 
-                          //import this without using compiler preprocessing.
+#include "lib/MPU6050.h"  //library is downloaded from online
+#include "lib/I2Cdev.h"   //library is downloaded from online
 
 
 MCP2515 mcp2515(2);
@@ -21,17 +20,18 @@ int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
 void setup() {
-  while (!Serial);
-  Serial.begin(9600);
-  
-  SPI.begin();               //Begins SPI communication
-  
+  while (!Serial) {
+		Serial.begin(9600);
+	}
+
+	//we can just use the Wire.h library for this arduino uno board, it works fine, so no preprocesser commands needed.
+  Wire.begin(); //for the i2c protocol devices (the mpu6050 accelerometer board)
+  SPI.begin();  //for the SPI protocol devices (the mcp2515 can board)
+	
+	Serial.println("Initializing SPI devices...");
   mcp2515.reset();
   mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ); //Sets CAN at speed 500KBPS and Clock 8MHz
   mcp2515.setNormalMode();
-
-  //we can just use the Wire.h library for this arduino uno board, it works fine, so no preprocesser commands needed.
-  Wire.begin();
 
   // initialize device
   Serial.println("Initializing I2C devices...");
@@ -44,6 +44,7 @@ void setup() {
 }
 
 void loop() {
+  
   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
   //cleaned up the code
   //the two for loops can be simplified into these two can messages being sent
