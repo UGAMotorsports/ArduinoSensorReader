620f21c226670792d39533393909e4f582ade5be
Bhargav Gudapati <bhargav_gudapati@yahoo.com>
2024-10-25 12:58:22 -0400

    self allocate buffer for moving average

--- AccelerometerTesting/MovingAverage.cpp
+++ AccelerometerTesting/MovingAverage.cpp
@@ -1,8 +1,10 @@
 
 #include "MovingAverage.h"
+#include "Arduino.h"
 
 MovingAverage::MovingAverage(int frameSize) {
   this->frameSize = frameSize;
+  this->frame = (short*)malloc(frameSize * sizeof(short));
   this->total = 0;
 }
 
